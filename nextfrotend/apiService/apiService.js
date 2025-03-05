06f0b085e8cb87cdac3ae028595b3bfa7be66904
fahim <fahimahamedweb@gmail.com>
2025-03-06 00:31:13 +0600

    graphql setup done

---
+++ nextfrotend/apiService/apiService.js
@@ -0,0 +1,61 @@
+import { cache } from "react";
+import { request } from "graphql-request";
+import {
+  homePageQuery,
+  postDetailsQuery,
+  postsPathQuery,
+  postsQuery,
+} from "./query";
+const graphqlUrl = process?.env?.NEXT_PUBLIC_GRAPHQL_URL;
+
+const loadHomePage = cache(async () => {
+  const query = homePageQuery;
+  const variables = {};
+  try {
+    const response = await request(graphqlUrl, query, variables);
+    return response?.homePage?.data?.attributes;
+  } catch (error) {
+    console.error("Error fetching data from Strapi:", error);
+    return null;
+  }
+});
+
+const loadPosts = cache(async () => {
+  const query = postsQuery;
+  const variables = {};
+  try {
+    const response = await request(graphqlUrl, query, variables);
+    return response?.posts?.data;
+  } catch (error) {
+    console.error("Error fetching data from Strapi:", error);
+    return null;
+  }
+});
+
+const loadPostDetails = cache(async (slug) => {
+  const query = postDetailsQuery;
+  const variables = {
+    slug,
+  };
+  try {
+    const response = await request(graphqlUrl, query, variables);
+    return response?.posts?.data[0];
+  } catch (error) {
+    console.error("Error fetching data from Strapi:", error);
+    return null;
+  }
+});
+
+const loadPostsPath = cache(async () => {
+  const query = postsPathQuery;
+  const variables = {};
+  try {
+    const response = await request(graphqlUrl, query, variables);
+    return response?.posts?.data;
+  } catch (error) {
+    console.error("Error fetching data from Strapi:", error);
+    return null;
+  }
+});
+
+export { loadHomePage, loadPosts, loadPostDetails, loadPostsPath };
---
+++ nextfrotend/app/page.js
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import { loadHomePage, loadPosts } from "@/apiService/apiService";
+
+import Link from "next/link";
+import imageUrl from "@/utils/generate-image-url";
+
+export default async function Home() {
+  const homePageData = await loadHomePage();
+  const postsData = await loadPosts();
+
+  return (
+    <div className="container mt-5">
+      <div className="content">
+        <h1>{homePageData?.title}</h1>
+        <div className="row">
+          {postsData?.map((post, index) => {
+            return (
+              <div className="col-4" key={"post" + index}>
+                <div className="card">
+                  <div className="card-body">
+                    <Image
+                      src={imageUrl(
+                        post?.attributes?.image?.data?.attributes?.url
+                      )}
+                      alt={"post details"}
+                      width={350}
+                      height={250}
+                      style={{
+                        width: "100%",
+                        height: "auto",
+                      }}
+                    />
+                    <h5 className="card-title">{post?.attributes?.title}</h5>
+                    <p className="card-text">
+                      {post?.attributes?.shortDescription}
+                    </p>
+                    <Link
+                      href={`/${post?.attributes?.slug}`}
+                      className="card-link"
+                    >
+                      Continue Reading
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
